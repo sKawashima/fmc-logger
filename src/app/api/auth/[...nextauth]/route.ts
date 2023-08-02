eb5556bdb1620cf1f11f30c2b3386458f3821797
sKawashima <bysk.333@gmail.com>
2023-08-02 21:57:39 +0900

    🔥 chore(.env.development): remove unused NEXTAUTH_URL environment variable
    ✨ feat(route.ts): add NextAuth route for authentication with Google provider
    
    🔧 fix(page.tsx): add LoginButton and LogoutButton components to the page
    
    ✨ feat(LoginButton.tsx): create LoginButton and LogoutButton components for signing in and signing out

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,23 @@
+import NextAuth, { NextAuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+type ClientType = {
+  clientId: string;
+  clientSecret: string;
+};
+
+const authOptions: NextAuthOptions = {
+  debug: true,
+  session: { strategy: "jwt" },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    } as ClientType),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { LoginButton, LogoutButton } from "@/component/molecules/LoginButton";
+
 export default function Home() {
   return (
+    <div>
     <p>hello world</p>
+    <LoginButton />
+      <LogoutButton />
+    </div>
   )
 }
