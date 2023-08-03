6081149a46603e0c03c0f584d0770db87bffcb0d
sKawashima <bysk.333@gmail.com>
2023-08-03 12:59:47 +0900

    🔀 chore(package.json): add dayjs package as a dependency to handle date and time operations
    🔄 refactor(page.tsx): replace createFmcScrambleToNormalScramble function with makeTodaysScramble function to generate today's scramble and display it on the page
    
    🔄 refactor(makeScramble.ts): remove createFmcScrambleToNormalScramble function and replace it with makeTodaysScramble function to generate today's scramble using Scrambo library and cube notation normalizer
    
    ✨ feat(scramble.ts): add makeTodaysScramble function to generate today's scramble using Scrambo library, cube notation normalizer, and Prisma ORM. If a scramble for today already exists in the database, retrieve it. Otherwise, create a new scramble and store it in the database.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
 import { LoginButton, LogoutButton } from '@/components/molecules/LoginButton'
 import { authOptions } from '@/resources/options'
-import { createFmcScrambleToNormalScramble } from '@/services/makeScramble'
+import { makeTodaysScramble } from '@/services/scramble'
 import { getServerSession } from 'next-auth'
 
 export default async function Home() {
   const session = await getServerSession(authOptions)
   const user = session?.user
 
+  const todayScramble = await makeTodaysScramble()
+
   return (
     <div>
       <p>hello {user ? user?.name : 'world'}</p>
       {user ? <LogoutButton /> : <LoginButton />}
-      <p>Scramble: {createFmcScrambleToNormalScramble()}</p>
+      <p>Todays Scramble: {todayScramble.scramble}</p>
     </div>
   )
 }
---
+++ src/services/scramble.ts
@@ -0,0 +1,37 @@
+import Scrambo from 'scrambo'
+import cubeNotationNormalizer from 'cube-notation-normalizer'
+import { PrismaClient } from '@prisma/client'
+import dayjs from 'dayjs'
+
+const threeByThree = new Scrambo().type('333')
+
+export const createFmcScrambleToNormalScramble = () => {
+  let scramble = threeByThree.get(1)[0]
+  while (
+    `R' U' F ${scramble} R' U' F`.length !==
+    cubeNotationNormalizer(`R' U' F ${scramble} R' U' F`).length
+  ) {
+    scramble = threeByThree.get(1)[0]
+  }
+
+  return `R' U' F ${scramble} R' U' F`
+}
+
+export const makeTodaysScramble = async () => {
+  const prisma = new PrismaClient()
+  const todayScramble =
+    (await prisma.scramble.findFirst({
+      where: {
+        createdAt: {
+          gte: dayjs().startOf('day').toDate(),
+          lt: dayjs().endOf('day').toDate(),
+        },
+      },
+    })) ||
+    (await prisma.scramble.create({
+      data: {
+        scramble: createFmcScrambleToNormalScramble(),
+      },
+    }))
+  return todayScramble
+}
