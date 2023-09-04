f4b7fd17bedb020a1fc6e9c38a786cbcf924d6a4
sKawashima <bysk.333@gmail.com>
2023-09-04 23:54:57 +0900

    📦 chore(tailwind.config.js): add tailwind configuration file
    The tailwind.config.js file is added to the project to configure the Tailwind CSS framework. This file includes the necessary imports, content, theme, dark mode, and plugins configuration for the Tailwind CSS framework.

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+import { nextui } from "@nextui-org/react";
+
+export const content = [
+  "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+];
+export const theme = {
+  extend: {},
+};
+export const darkMode = "class";
+export const plugins = [nextui()];
