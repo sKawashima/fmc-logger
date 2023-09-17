2abad6de93832ef3f88aed0230646b2ef4b29ae0
sKawashima <bysk.333@gmail.com>
2023-09-17 21:48:59 +0900

    🔨 refactor(SetUserIdForm.tsx): extract form submission logic into a separate function for code reusability and readability ✨ feat(SetUserIdForm.tsx): handle form submission using the new onSubmit function to improve code structure and maintainability

--- src/components/organisms/SetUserIdForm.tsx
+++ src/components/organisms/SetUserIdForm.tsx
@@ -8,6 +8,28 @@ export const UpdateUserShowIdForm = () => {
   const router = useRouter()
   const [error, setError] = useState<string | null>(null)
 
+  const onSubmit = async () => {
+    const userIdForShowInputElement = document.getElementById(
+      'userIdForShow',
+    ) as HTMLInputElement
+    if (!userIdForShowInputElement) return
+
+    const responce = await fetch('/api/user/setId', {
+      method: 'POST',
+      body: JSON.stringify({
+        showId: userIdForShowInputElement.value,
+      }),
+    })
+
+    if (responce.status === 200) {
+      router.push('/')
+    } else if (responce.status === 409) {
+      setError('そのIDは既に使われています')
+    } else {
+      setError('送信に失敗しました')
+    }
+  }
+
   return (
     <Pane>
       <TextInputField
@@ -16,31 +38,7 @@ export const UpdateUserShowIdForm = () => {
         isInvalid={Boolean(error)}
         validationMessage={error}
       />
-      <Button
-        appearance="primary"
-        // TODO: Refactor Method
-        onClick={async () => {
-          const userIdForShowInputElement = document.getElementById(
-            'userIdForShow',
-          ) as HTMLInputElement
-          if (!userIdForShowInputElement) return
-
-          const responce = await fetch('/api/user/setId', {
-            method: 'POST',
-            body: JSON.stringify({
-              showId: userIdForShowInputElement.value,
-            }),
-          })
-
-          if (responce.status === 200) {
-            router.push('/')
-          } else if (responce.status === 409) {
-            setError('そのIDは既に使われています')
-          } else {
-            setError('送信に失敗しました')
-          }
-        }}
-      >
+      <Button appearance="primary" onClick={onSubmit}>
         submit
       </Button>
     </Pane>
