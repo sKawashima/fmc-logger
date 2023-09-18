e8ae5adfd08068f26aa39a6161d88fee52020bbb
sKawashima <bysk.333@gmail.com>
2023-09-18 13:34:19 +0900

    🔧 fix(GlobalHeader.tsx): import useRouter from 'next/navigation' to fix the missing import error ✨ feat(GlobalHeader.tsx): add onClick event to the heading to navigate to the home page when clicked

--- src/components/organisms/GlobalHeader.tsx
+++ src/components/organisms/GlobalHeader.tsx
@@ -4,6 +4,7 @@ import { Heading, Pane, majorScale } from 'evergreen-ui'
 import { LoginButton } from '../molecules/LoginButton'
 import { UserAvatar } from '../molecules/UserAvatar'
 import { User } from '@/services/user'
+import { useRouter } from 'next/navigation'
 
 type Props = {
   user: User | null
@@ -11,6 +12,7 @@ type Props = {
 
 export const GlobalHeader = (props: Props) => {
   const isLogin = Boolean(props.user)
+  const router = useRouter()
 
   return (
     <header>
@@ -22,7 +24,15 @@ export const GlobalHeader = (props: Props) => {
         paddingX={majorScale(2)}
         paddingY={majorScale(1)}
       >
-        <Heading size={600}>FMC Logger</Heading>
+        <Heading
+          size={600}
+          cursor="pointer"
+          onClick={() => {
+            router.push('/')
+          }}
+        >
+          FMC Logger
+        </Heading>
         {isLogin ? <UserAvatar user={props.user} /> : <LoginButton />}
       </Pane>
     </header>
