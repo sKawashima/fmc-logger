b9867e0c7ec042afd1d1354129d511da1da59eab
sKawashima <bysk.333@gmail.com>
2023-09-18 21:17:17 +0900

    🔨 refactor(page.tsx): replace existing content with TopTemplate component for better code organization and reusability 🔨 refactor(GlobalBodyWrapper.tsx): add minHeight property to GlobalBodyWrapper component to ensure content is visible even when there is not enough content to fill the entire viewport 🔨 refactor(TopTemplate.tsx): add logic to fetch today's scramble and display a link to challenge it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,5 @@
-import { LoginButton, LogoutButton } from '@/components/molecules/LoginButton'
-import { makeTodaysScramble } from '@/services/scramble'
-import { getUser } from '@/services/user'
-import Link from 'next/link'
-import { redirect } from 'next/navigation'
+import { TopTemplate } from '@/components/templates/TopTemplate'
 
 export default async function Home() {
-  const user = await getUser()
-  if (user && !user.showId) redirect('/user/setId')
-
-  const todayScramble = await makeTodaysScramble()
-
-  return (
-    <div>
-      <p>
-        hello{' '}
-        {user ? (
-          <Link href={`/user/${user.showId}`}>{user.name}</Link>
-        ) : (
-          'world'
-        )}
-      </p>
-      {user ? <LogoutButton /> : <LoginButton />}
-      <Link href={`/scramble/${todayScramble.id}`}>Todays Scramble</Link>
-    </div>
-  )
+  return <TopTemplate />
 }
--- src/components/templates/TopTemplate.tsx
+++ src/components/templates/TopTemplate.tsx
@@ -1,10 +1,14 @@
-export const TopTemplate = () => {
+import { makeTodaysScramble } from '@/services/scramble'
+import Link from 'next/link'
+
+export const TopTemplate = async () => {
+  const todayScramble = await makeTodaysScramble()
+
   return (
     <div>
-      <main>
-        <h1>TopTemplate</h1>
-      </main>
-      {/* <GlobalFooter /> */}
+      <Link href={`/scramble/${todayScramble.id}`}>
+        Challenge Today&rsquo;s Scramble
+      </Link>
     </div>
   )
 }
