7c380813bcaa7c4b63e05162110084e0eee076fd
sKawashima <bysk.333@gmail.com>
2025-07-06 23:41:28 +0900

    feat: add gradient blur effect to image edges

--- src/components/templates/TopTemplate.tsx
+++ src/components/templates/TopTemplate.tsx
@@ -9,7 +9,11 @@ export const TopTemplate = async () => {
   return (
     <>
       <div className="flex justify-center">
-        <Image src={imageCharacter} alt="Character" width={400} />
+        <div className="relative">
+          <Image src={imageCharacter} alt="Character" width={400} />
+          <div className="absolute inset-0 bg-gradient-to-r from-white via-transparent to-white pointer-events-none" />
+          <div className="absolute inset-0 bg-gradient-to-b from-white via-transparent to-white pointer-events-none" />
+        </div>
       </div>
       <TopTodayScrambleSection linkUrl={`/scramble/${todayScramble.id}`} />
     </>
